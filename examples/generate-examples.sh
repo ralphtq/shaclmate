7bb1c9f46b85964df2e25bdd1dfb304582f15808
Minor Gordon <minorg@server.fake>
2025-01-01 13:41:08 -0500

    generate SKOS

--- examples/generate-examples.sh
+++ examples/generate-examples.sh
@@ -10,19 +10,20 @@ format_rdf() {
 }
 
 # Kitchen sink
-format_rdf $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl
-./packages/cli/cli.sh show-ast-json $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl >examples/kitchen-sink/ast.json
-./packages/cli/cli.sh generate $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl >examples/kitchen-sink/generated.ts
+#format_rdf $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl
+#./packages/cli/cli.sh show-ast-json $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl >examples/kitchen-sink/ast.json
+#./packages/cli/cli.sh generate $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl >examples/kitchen-sink/generated.ts
 
 # SKOS
-#SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH="../kos-kit/skos-shacl/shapes/skos-shaclmate.shacl.ttl"
-#if [ -f "$SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH" ] ; then
-#  cp $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH $PWD/examples/skos/skos.shapes.ttl
-#  echo "copied SKOS shapes from original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH"
-#else
-#  echo "SKOS shapes original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH not found, using copy"
-#fi
-#./packages/cli/cli.sh show-ast-json $PWD/examples/skos/skos.shapes.ttl >examples/skos/ast.json
-#./packages/cli/cli.sh generate $PWD/examples/skos/skos.shapes.ttl >examples/skos/generated.ts
+SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH="../kos-kit/skos-shacl/shapes/skos-shaclmate.shacl.ttl"
+if [ -f "$SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH" ] ; then
+  cp $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH $PWD/examples/skos/skos.shapes.ttl
+  echo "copied SKOS shapes from original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH"
+else
+  echo "SKOS shapes original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH not found, using copy"
+fi
+format_rdf $PWD/examples/skos/skos.shapes.ttl
+./packages/cli/cli.sh show-ast-json $PWD/examples/skos/skos.shapes.ttl >examples/skos/ast.json
+./packages/cli/cli.sh generate $PWD/examples/skos/skos.shapes.ttl >examples/skos/generated.ts
 
 npm exec biome -- check --write --unsafe $PWD/examples
