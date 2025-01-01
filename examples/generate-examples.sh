e49eda0015d1da0fcd5bfca677850f286d08e83b
Minor Gordon <minorg@server.fake>
2025-01-01 13:45:51 -0500

    regenerate SKOS

--- examples/generate-examples.sh
+++ examples/generate-examples.sh
@@ -7,6 +7,7 @@ cd "$(dirname "$0")/.."
 format_rdf() {
   rapper -i turtle -o turtle -q $1 >temp.ttl
   mv -f temp.ttl $1
+  echo "formatted $1"
 }
 
 # Kitchen sink
@@ -17,12 +18,12 @@ format_rdf() {
 # SKOS
 SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH="../kos-kit/skos-shacl/shapes/skos-shaclmate.shacl.ttl"
 if [ -f "$SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH" ] ; then
+  format_rdf $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH
   cp $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH $PWD/examples/skos/skos.shapes.ttl
   echo "copied SKOS shapes from original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH"
 else
   echo "SKOS shapes original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH not found, using copy"
 fi
-format_rdf $PWD/examples/skos/skos.shapes.ttl
 ./packages/cli/cli.sh show-ast-json $PWD/examples/skos/skos.shapes.ttl >examples/skos/ast.json
 ./packages/cli/cli.sh generate $PWD/examples/skos/skos.shapes.ttl >examples/skos/generated.ts
 
