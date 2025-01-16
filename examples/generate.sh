932c4f7b1c40c7b1468f7b316c92774ce593d3a7
Minor Gordon <minorg@server.fake>
2025-01-16 17:34:00 -0500

    shapes.ttl -> shaclmate.ttl

--- examples/generate.sh
+++ examples/generate.sh
@@ -11,20 +11,20 @@ format_rdf() {
 }
 
 # Kitchen sink
-format_rdf $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl
-./packages/cli/cli.sh show-ast-json $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl >examples/kitchen-sink/ast.json
-./packages/cli/cli.sh generate $PWD/examples/kitchen-sink/kitchen-sink.shapes.ttl >examples/kitchen-sink/generated.ts
+format_rdf $PWD/examples/kitchen-sink/kitchen-sink.shaclmate.ttl
+./packages/cli/cli.sh show-ast-json $PWD/examples/kitchen-sink/kitchen-sink.shaclmate.ttl >examples/kitchen-sink/ast.json
+./packages/cli/cli.sh generate $PWD/examples/kitchen-sink/kitchen-sink.shaclmate.ttl >examples/kitchen-sink/generated.ts
 
 # SKOS
 #SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH="../kos-kit/skos-shacl/shapes/skos-shaclmate.shacl.ttl"
 #if [ -f "$SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH" ] ; then
 #  # format_rdf $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH
-#  cp $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH $PWD/examples/skos/skos.shapes.ttl
+#  cp $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH $PWD/examples/skos/skos.shaclmate.ttl
 #  echo "copied SKOS shapes from original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH"
 #else
 #  echo "SKOS shapes original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH not found, using copy"
 #fi
-#./packages/cli/cli.sh show-ast-json $PWD/examples/skos/skos.shapes.ttl >examples/skos/ast.json
-#./packages/cli/cli.sh generate $PWD/examples/skos/skos.shapes.ttl >examples/skos/generated.ts
+#./packages/cli/cli.sh show-ast-json $PWD/examples/skos/skos.shaclmate.ttl >examples/skos/ast.json
+#./packages/cli/cli.sh generate $PWD/examples/skos/skos.shaclmate.ttl >examples/skos/generated.ts
 
 npm exec biome -- check --write --unsafe $PWD/examples
