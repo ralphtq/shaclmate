b6becd753fce0b654f1748a3d37a66699147d1cd
Minor Gordon <minorg@server.fake>
2025-01-22 10:14:42 -0500

    delete skos example

--- examples/generate.sh
+++ examples/generate.sh
@@ -10,6 +10,7 @@ format_rdf() {
   echo "formatted $1"
 }
 
+# Forms
 #format_rdf $PWD/examples/forms/forms.shaclmate.ttl
 #./packages/cli/cli.sh generate $PWD/examples/forms/forms.shaclmate.ttl >examples/forms/src/generated.ts
 
@@ -17,17 +18,5 @@ format_rdf() {
 format_rdf $PWD/examples/kitchen-sink/kitchen-sink.shaclmate.ttl
 ./packages/cli/cli.sh show-ast-json $PWD/examples/kitchen-sink/kitchen-sink.shaclmate.ttl >examples/kitchen-sink/ast.json
 ./packages/cli/cli.sh generate $PWD/examples/kitchen-sink/kitchen-sink.shaclmate.ttl >examples/kitchen-sink/generated.ts
-
-# SKOS
-#SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH="../kos-kit/skos-shacl/shapes/skos-shaclmate.shacl.ttl"
-#if [ -f "$SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH" ] ; then
-#  # format_rdf $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH
-#  cp $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH $PWD/examples/skos/skos.shaclmate.ttl
-#  echo "copied SKOS shapes from original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH"
-#else
-#  echo "SKOS shapes original file $SKOS_SHAPES_TTL_ORIGINAL_FILE_PATH not found, using copy"
-#fi
-#./packages/cli/cli.sh show-ast-json $PWD/examples/skos/skos.shaclmate.ttl >examples/skos/ast.json
-#./packages/cli/cli.sh generate $PWD/examples/skos/skos.shaclmate.ttl >examples/skos/generated.ts
 
 npm exec biome -- check --write --unsafe $PWD/examples
