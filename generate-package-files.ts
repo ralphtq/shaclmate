dacd4d58f3f8df0821d9e9f9cfa0aa37a668880f
Minor Gordon <minorg@server.fake>
2025-03-24 16:59:00 -0400

    bump rdfjs-resource version

--- generate-package-files.ts
+++ generate-package-files.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import url from "node:url";
 import { stringify as stringifyYaml } from "yaml";
 
-const VERSION = "2.0.20";
+const VERSION = "2.0.21";
 
 type PackageName =
   | "cli"
@@ -42,7 +42,7 @@ const externalDependencyVersions = {
   n3: "^1.21.3",
   pino: "^9.1.0",
   "purify-ts": "^2.1.0",
-  "rdfjs-resource": "1.0.17",
+  "rdfjs-resource": "1.0.18",
 };
 
 // Packages should be topologically sorted
